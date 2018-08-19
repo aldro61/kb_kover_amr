835a61adb642ce87fa5d69f8f8349e2364b9f898
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2018-08-19 00:30:55 -0400

    trying to get registration to work

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -17,7 +17,9 @@ elif [ "${1}" = "async" ] ; then
   sh ./scripts/run_async.sh
 elif [ "${1}" = "init" ] ; then
   echo "Initialize module"
-  cp -r ./data/models /data/
+  mkdir /data/kover_amr
+  mkdir /data/kover_amr/0.0.1
+  cp -r ./data/models /data/kover_amr/0.0.1
   touch /data/__READY__
 elif [ "${1}" = "bash" ] ; then
   bash
