4260a5849efd5c82d252f3e84d44b4ccda13e5c9
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2018-08-24 18:12:16 -0400

    testing modal dialog

--- lib/kover_amr/reports.py
+++ lib/kover_amr/reports.py
@@ -8,6 +8,42 @@ from urllib import quote
 MODEL_BASE_URL = "https://github.com/aldro61/kb_kover_amr/tree/master/data/models/{}/{}/{}/README.md"
 
 
+def generate_explanation_dialog(assembly, antibiotic, species, algorithm, predicted_label, explanation):
+    modal_id = str(hash(assembly + antibiotic + species))
+
+    model_url = MODEL_BASE_URL.format(algorithm, quote(species.lower()), quote(antibiotic.lower().replace(" ", "_")))
+
+    title = assembly.title() + " - " + antibiotic.title()
+
+    explanation = "\n".join(explanation)
+
+    modal_template = \
+"""
+<div class="modal fade" id="{0!s}" tabindex="-1" role="dialog" aria-labelledby="{0!s}Title" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="{0!s}Title">{1!s}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Prediction: {2!s}<br /><br />
+        Reason:<br />
+        {3!s}
+      </div>
+      <div class="modal-footer">
+        <a href="{4!s}" target="_blank" class="btn btn-primary">View model</a>
+      </div>
+    </div>
+  </div>
+</div>
+"""
+
+    return modal_id, modal_template.format(modal_id, title, predicted_label, explanation, model_url)
+
+
 def generate_html_prediction_report(predictions, species):
 
     antibiotics = predictions[predictions.keys()[0]]["scm"].keys()
@@ -23,26 +59,7 @@ def generate_html_prediction_report(predictions, species):
 </head>
 <body>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+{}
 
 {}
 
@@ -57,6 +74,7 @@ def generate_html_prediction_report(predictions, species):
 """
 
     prediction_table_rows = []
+    explanation_dialogs = []
     for assembly in predictions.keys():
         scm_row = \
 """
@@ -65,9 +83,9 @@ def generate_html_prediction_report(predictions, species):
     <td>SCM</td>
     {}
 </tr>""".format(assembly, "\n".join(["<td class='{}'><button type='button' class='btn btn-link' data-toggle='modal' data-target='#exampleModalCenter'>{}</button></td>".format("table-danger" if predictions[assembly]["scm"][a]["label"] == "resistant" else "table-success",
-                                                                                                      #MODEL_BASE_URL.format("scm", quote(species.lower()), quote(a.lower().replace(" ", "_"))),
                                                                                                       predictions[assembly]["scm"][a]["label"]) for a in antibiotics]))
         prediction_table_rows.append(scm_row)
+        explanation_dialogs += [generate_explanation_dialog(assembly, a, species, "scm", predictions[assembly]["scm"][a]["label"], predictions[assembly]["scm"][a]["why"]) for a in antibiotics]
 
         cart_row = \
 """
@@ -97,4 +115,6 @@ def generate_html_prediction_report(predictions, species):
 """.format("\n".join(["<th scope='col'>" + a + "</th>" for a in antibiotics]),
            "\n".join(prediction_table_rows))
 
-    return report.format(result_table)
+    explanation_dialogs = "\n".join(explanation_dialogs)
+
+    return report.format(result_table, explanation_dialogs)
