f76ac32f8c18a7f8807d8413ab7e03c6f4fea8fd
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2018-08-24 10:27:00 -0400

    report test

--- lib/kover_amr/reports.py
+++ lib/kover_amr/reports.py
@@ -12,9 +12,19 @@ def generate_html_prediction_report(predictions):
 <html>
 <head>
     <title>Kover Antimicrobial resistance prediction report</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 {}
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
 </body>
 </html>
 """
@@ -28,6 +38,16 @@ def generate_html_prediction_report(predictions):
         {}
     </tr>
 </table>
+
+<main role="main" class="container">
+
+    <div class="starter-template">
+    <h1>Bootstrap starter template</h1>
+    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+    </div>
+
+</main>
+
 """.format("\n".join(["<th>" + a + "</th>" for a in antibiotics]))
 
     return report.format(result_table)
