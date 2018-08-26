1c315a5a0b67e91652669f49825221c2609cb4c2
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2018-08-26 19:54:37 -0400

    tooltips

--- lib/kover_amr/reports.py
+++ lib/kover_amr/reports.py
@@ -121,6 +121,12 @@ def generate_html_prediction_report(predictions, species):
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+</script>
+
 </body>
 </html>
 """
